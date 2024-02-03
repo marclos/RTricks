d1c8ec7a44c82201a96657edda9cdd80803cc7f6
marclos <marc.loshuertos@pomona.edu>
2024-02-03 02:25:29 -0800

    3 and 4 started

---
+++ 04_Regional_Climate_Trends/Guides/Guide4.R
@@ -0,0 +1,49 @@
+# Guide1.R
+
+# function to read(inventory.active.oldest) and subset(my.state)
+readInventory.fun<-function(filename, my.state){
+  inventory.active.oldest <- read.csv(filename)
+  my.inventory = subset(inventory.active.oldest, STATE == my.state)
+  return(my.inventory)
+}
+
+# Download All Weather Station Data and Read Into R
+downloadStations.fun <- function(datafolder, my.inventory=my.inventory){
+  for(i in 1:nrow(my.inventory)){
+    url = paste0("https://www.ncei.noaa.gov/pub/data/ghcn/daily/by_station/", 
+      my.inventory$ID[i], ".csv.gz")
+  
+  download.file(url, paste0(datafolder, my.inventory$ID[i], ".csv.gz"), 
+    quiet = FALSE, mode = "w", cacheOK = TRUE)
+  
+#  assign(paste0("station", i), 
+#    read.csv(gzfile(paste0(datafolder,my.inventory$ID[i], ".csv.gz")), 
+#      header=FALSE))
+  
+  print(paste("Index (Loop) ", i, " Completed.")) # Print Index Number 
+  print("Think about something you are grateful for today!")  
+  
+} # LOOP END
+
+  StationList.df <- lapply( # Read All Files into R
+      list.files(datafolder, full.names=TRUE, pattern = "\\.gz$"), 
+        read.csv, header=FALSE)
+  # Fix Variable Names based on NOAA Documentation
+  colnames <- c("ID", "DATE", "ELEMENT", "VALUE", 
+                "M-FLAG", "Q-FLAG", "S-FLAG", "OBS-TIME")
+  lapply(StationList.df, setNames, colnames)
+  
+  for (i in seq_along(StationList.df)) {
+    filename = paste0(datafolder, my.inventory$ID[i], ".csv")
+    write.csv(StationList.df[[i]], filename, row.names = FALSE)
+  }
+}
+
+
+
+
+
+
+
+
+
