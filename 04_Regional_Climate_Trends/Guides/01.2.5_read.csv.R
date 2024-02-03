fdf9c4da0f36c3d14c06e29582577e2413b175c2
marclos <marc.loshuertos@pomona.edu>
2024-02-02 20:08:29 -0800

    clean up

--- 04_Regional_Climate_Trends/Guides/01.2.5_read.csv.R
+++
@@ -1,7 +0,0 @@
-# knit rchunktest.R
-x = summary(cars)
-
-# Path: 04_Regional_Climate_Trends/01_Station_Data.R
-# here::here("04_Regional_Climate_Trends", my.stations$ID[i])
-my.stations = subset(stations.active.oldest, STATE == my.state)
-
