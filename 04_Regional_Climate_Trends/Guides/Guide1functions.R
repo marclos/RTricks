b47c0519967c2f714e2b773e3eb26a78facacbe6
marclos <marc.loshuertos@pomona.edu>
2024-02-15 20:00:39 -0800

    minor corrections to guide 1

--- 04_Regional_Climate_Trends/Guides/Guide1functions.R
+++ 04_Regional_Climate_Trends/Guides/Guide1functions.R
@@ -1,5 +1,5 @@
 # Guide1functions.R
-# Updated: 2020-02-09
+# Updated: 2020-02-15
 
 #-------------------------------------------------------------------------------
 # function to read(inventory.active.oldest) and subset criteria (my.state)
@@ -11,6 +11,7 @@ readInventory.fun<-function(filename, my.state){
 
 #-------------------------------------------------------------------------------
 # Download All Weather Station Data and Read Into R
+# This method relies on a loop that seems to create problems. Good ridance!
 # datafolder = "/home/mwl04747/RTricks/04_Regional_Climate_Trends/Data/SP24/"
 downloadStationsOLD.fun <- function(datafolder, my.inventory=my.inventory){
   # Loop through all stations and download data
