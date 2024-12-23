7e32c49712ced2e9d7a2bd65710b66acc7c797a7
marclos <marc.loshuertos@pomona.edu>
2024-12-22 22:32:43 -0800

    keeling three gases

--- 03_Keeling_GWP/keeling_GWP.R
+++ 03_Keeling_GWP/keeling_GWP.R
@@ -8,7 +8,17 @@ N2O <- read.csv("https://gml.noaa.gov/webdata/ccgg/trends/n2o/n2o_mm_gl.csv", sk
 names(CO2)
 names(CH4)
 
+par(las=1, mar=c(5, 4, 4, 10) + 0.1)
+years = c(1958, 2025)
+plot(average ~ decimal.date, data=CO2, pch=20, col='grey', cex=.2, las=1, ylab="CO2 (ppm v/v)", ylim=c(300, 430), xlim=years, xlab="Year")
+par(new = TRUE)
+plot(average ~ decimal, data=CH4, pch=20, col='blue', cex=.2, axes=FALSE, xlim=years, xlab=NA, ylab=NA)
+axis(4, col = "blue", col.ticks='blue', col.axis='blue')
+mtext("CH4 (ppb v/v)", side = 4, line = 3, las=0, col='blue')
+par(new = TRUE)
+plot(average ~ decimal, data=N2O, pch=20, col='red', cex=.2, axes=FALSE, xlim=years, xlab=NA, ylab=NA, ylim=c(310, 340))
+axis(4, col = "red", line=5, col.ticks='red', col.axis='red')
+mtext("N2O (ppb v/v)", side = 4, line = 8, las=0, col='red')
 
-plot(average ~ decimal.date, data=CO2, pch=20, col='grey', cex=.3, las=1, ylab="ppm (v/v)", ylim=c(300, 430))
-points(average/5 ~ decimal, data=CH4, pch=20, col='blue', cex=.3)
-points(average/1 ~ decimal, data=N2O, pch=20, col='red', cex=.3)
+
+
