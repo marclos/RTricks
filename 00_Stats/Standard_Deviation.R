efbd2fcedad965f07b718ea24b7098105412a4a5
marclos <marc.loshuertos@pomona.edu>
2025-02-04 12:06:06 -0800

    added SD

---
+++ 00_Stats/Standard_Deviation.R
@@ -0,0 +1,24 @@
+R1 = c(4.5, 2, 10, 7, 10, 10, 6.5, 4, 4, 9, 13, 5, 4.5, 6, 8, 7.5, 4, NA)
+R2 = c(10, 6, 8, 8, NA,  8, 8, 4.5, 8, 3, NA, 10, 10, 9.5, 11, NA, 6, 9)
+
+length(R1)
+
+mean(R1)
+
+R1.mean = mean(R1, na.rm = TRUE); R1.mean
+
+R1.nonmissing = R1[!is.na(R1)]; R1.nonmissing
+
+N = length(R1.nonmissing); N
+
+R1.residual = R1.mean-R1.nonmissing; R1.residual
+
+plot(R1.residual)
+
+sum(R1.residual, na.rm=TRUE)
+
+SS = sum(R1.residual^2, na.rm=TRUE)
+
+sqrt(SS/N)
+  
+sd(R1.nonmissing)
