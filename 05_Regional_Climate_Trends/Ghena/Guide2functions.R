ea6bcc7a0fdcdaa2ae5020fb4dd293b448df3836
marclos <marc.loshuertos@pomona.edu>
2025-02-20 13:08:45 -0800

    update for reflection

---
+++ 05_Regional_Climate_Trends/Ghena/Guide2functions.R
@@ -0,0 +1,187 @@
+# Guide2functions.R
+# Updated: 2024-02-15
+
+#-------------------------------------------------------------------------------
+ReadStations2.fun <- function(datafolder){
+  my.stations = read.csv(paste0(datafolder, "my.inventory.csv"))
+# Fix Variable Names based on NOAA Documentation
+colnames <- c("ID", "DATE", "ELEMENT", "VALUE", 
+              "M-FLAG", "Q-FLAG", "S-FLAG", "OBS-TIME")
+for (i in 1:nrow(my.stations)){
+  assign(my.stations$ID[i], 
+  read.csv(paste0(datafolder, noquote(my.stations$ID[i]), ".csv"), header=TRUE, col.names=colnames), envir = parent.frame())
+}
+}
+
+#-------------------------------------------------------------------------------
+# number of observations for each dataframe
+# 2/8/2024 Not working yet.
+sortStations.fun <- function(my.inventory){
+  for(i in 1:nrow(my.inventory)){
+    x[i] <- nrow(my.inventory$ID[i])
+    print(x[i])
+    return(x)
+  }}
+
+
+#-------------------------------------------------------------------------------
+# function to fix date formats, etc.
+fixDates.fun <- function(station){
+  station$Ymd = as.Date(as.character(station$DATE), format = "%Y%m%d")
+  station$MONTH = as.numeric(format(station$Ymd, "%m"))
+  station$YEAR = as.numeric(format(station$Ymd, "%Y"))
+  return(station)
+}
+
+#-------------------------------------------------------------------------------
+# Data Coverage Function --Determine percent missing
+coverage.fun <- function(station, element="TMAX"){
+  Dates.all = data.frame(Ymd=seq.Date(from=min(station$Ymd), 
+                          to=max(station$Ymd), by="day"))
+  station.full = merge(Dates.all, station, all = TRUE)
+  station.coverage = 
+    sum(!is.na(station.full$VALUE[station.full$ELEMENT==element]))/
+  length(station.full$VALUE[station.full$ELEMENT==element])*100
+  return(round(station.coverage,2))
+  }
+
+
+#-------------------------------------------------------------------------------
+# Function to Convert/Transform Units
+fixValues.fun <- function(station){
+    station$VALUE = station$VALUE/10
+  return(station)
+}
+
+#-------------------------------------------------------------------------------
+# QA/QC Function
+QAQC.fun <- function(station){
+  par(mfrow=c(1,1)) # chnaged this from 3 to 1 because of window size issues
+  plot(VALUE ~ Ymd, data = subset(station, 
+      subset=ELEMENT=="PRCP"), type = "l", col = "blue", 
+    main = "Time Series of Daily PRCP", xlab = "Date", ylab = "PRCP (mm)")
+  plot(VALUE ~ Ymd, data = subset(station, subset=ELEMENT=="TMAX"), 
+      type = "l", col = "blue", 
+    main = "Time Series of Daily TMAX", xlab = "Date", ylab = "TMAX (C)")
+  plot(VALUE ~ Ymd, data = subset(station, 
+      subset=ELEMENT=="TMIN"), type = "l", col = "blue", 
+    main = "Time Series of Daily TMIN", xlab = "Date", ylab = "TMIN (C)") 
+  station = subset(station, Q.FLAG != "")
+  station = subset(station, M.FLAG != "")
+  station = subset(station, S.FLAG != "")
+  return(station)
+}
+
+#-------------------------------------------------------------------------------
+# Create Monthly Averages/Sums
+MonthlyValues.fun <- function(x){
+  x.TMAX.monthly = aggregate(VALUE ~ MONTH + YEAR, 
+        data = subset(x, ELEMENT == "TMAX"), mean)
+  names(x.TMAX.monthly) <- c("MONTH", "YEAR", "TMAX")
+  x.TMIN.monthly = aggregate(VALUE ~ MONTH + YEAR, 
+        data = subset(x, ELEMENT == "TMIN"), mean)
+  names(x.TMIN.monthly) <- c("MONTH", "YEAR", "TMIN")
+  x.PRCP.monthly = aggregate(VALUE ~ MONTH + YEAR, 
+        data = subset(x, ELEMENT == "PRCP"), sum)
+  names(x.PRCP.monthly) <- c("MONTH", "YEAR", "PRCP")
+  return(list(x.TMAX.monthly, x.TMIN.monthly, x.PRCP.monthly))
+}
+
+
+#-------------------------------------------------------------------------------
+# Function to Create Monthly Normals
+MonthlyNormals.fun <- function(x){
+  x.normals = subset(x, 
+    Ymd >= "1961-01-01" & Ymd <= "1990-12-31")  
+  x.TMAX.normals.monthly = aggregate(VALUE ~ MONTH, 
+    data = subset(x.normals, ELEMENT == "TMAX"), mean)
+  names(x.TMAX.normals.monthly) <- c("MONTH", "NORMALS")
+  x.TMIN.normals.monthly = aggregate(VALUE ~ MONTH, 
+    data = subset(x.normals, ELEMENT == "TMIN"), mean)
+  names(x.TMIN.normals.monthly) <- c("MONTH", "NORMALS")
+  x.PRCP.normals.month.year = aggregate(VALUE ~ MONTH + YEAR, 
+    data = subset(x.normals, ELEMENT == "PRCP"), sum)
+  x.PRCP.normals.monthly = aggregate(VALUE ~ MONTH, 
+    data = subset(x.PRCP.normals.month.year), mean)
+  names(x.PRCP.normals.monthly) <- c("MONTH", "NORMALS")
+  return(list(x.TMAX.normals.monthly, 
+              x.TMIN.normals.monthly, 
+              x.PRCP.normals.monthly))
+}
+
+#-------------------------------------------------------------------------------
+# Check on PRCP Values!
+#par(mfrow=c(1,1))
+#plot(VALUE ~ Ymd, data = subset(station1a, subset=ELEMENT=="PRCP"), type = "p", col = "blue", main = "Time Series of Daily PRCP", xlab = "Date", ylab = "PRCP (mm)", pch=20, cex=.2)
+#plot(NORMALS ~ MONTH, data = station1.normals[[3]], type = "p", col = "blue", main = "NORMALS PRCP", xlab = "MONTH", ylab = "PRCP (mm)") 
+#plot(PRCP ~ YEAR + MONTH, data = station1.monthly[[3]], type = "p", col = "blue",  main = "MONTHLY PRCP", xlab = "MONTH", ylab = "PRCP (mm)")
+
+
+#-------------------------------------------------------------------------------
+# Function to Calculate Monthly Anomalies
+MonthlyAnomalies.fun <- function(station.monthly, station.normals){
+for(i in seq_along(station.monthly)){
+  TMAX <- merge(station.monthly[[1]], station.normals[[1]], by = "MONTH")
+  TMAX$TMAX.a = TMAX$TMAX - TMAX$NORMALS
+  TMAX$Ymd = as.Date(paste(TMAX$YEAR, TMAX$MONTH, "01", sep = "-"))
+  TMIN <- merge(station.monthly[[2]], station.normals[[2]], by = "MONTH")
+  TMIN$TMIN.a = TMIN$TMIN - TMIN$NORMALS
+  TMIN$Ymd = as.Date(paste(TMIN$YEAR, TMIN$MONTH, "01", sep = "-"))
+  PRCP <- merge(station.monthly[[3]], station.normals[[3]], by = "MONTH")
+  PRCP$PRCP.a = PRCP$PRCP - PRCP$NORMALS 
+  PRCP$Ymd = as.Date(paste(PRCP$YEAR, PRCP$MONTH, "01", sep = "-"))
+  return(list(TMAX = TMAX, TMIN = TMIN, PRCP = PRCP))  
+  }
+}
+
+
+#-------------------------------------------------------------------------------
+##
+# Trouble Shooting Fuctions
+
+# Function to see if dataframe exists
+df_exists = function(name){
+  exists(name, envir = parent.frame())
+}
+
+# Function to see what dataframe names are in list
+df_names = function(df){
+  attr(df, "names")
+}
+
+# Function see dataframe head
+df_head = function(df){
+  lapply(df, head, 6)
+}
+
+# Test Trouble Shooting Functions
+# df_exists("USC00042294b")
+# df_names(USC00042294b)
+# df_head(USC00042294.anomalies)
+
+
+
+#-------------------------------------------------------------------------------
+# Function to Up R Environment
+## Write Anomaly Data to CSV
+## Remove functions from Guide 1 and 2 from environment
+## Remove station data, except anomaly data from environment
+
+## doesn't work yet
+SaveCleanUp.fun <- function(datapath){
+      anomalies.ls = ls(pattern="*.anomalies", envir = parent.frame())
+      save(list = anomalies.ls, file=paste0(datafolder, "anomalies", ".RData"))      
+      #rm(list = ls(pattern="fun"), envir = parent.frame())
+      #rm(list = station.obj, envir = parent.frame())
+}
+
+# test function
+# SaveCleanUp.fun(datafolder)
+
+#-------------------------------------------------------------------------------
+# Function to Save Anomaly Data
+
+# I think this is a good idea, but the path issues are complicated, so 
+# I haven't implemented it yet. 
+
+#save(USC00042294.anomalies, file=paste0(datafolder, "anomalies", ".RData"))
---
+++ 05_Regional_Climate_Trends/Ghena/Guide3functions.R
@@ -0,0 +1,306 @@
+# Guide3functions.R
+# Updated: 2024-02-15
+
+# Read Data into List of Dataframes
+# This method of using data required when I Rstudio to knit code
+# which creates its own environment and the objects created in the 
+# console are not available to the knitted code. Just learned 
+# this from the discussion board after years of some minor 
+# frustration.
+
+#load(file=paste0(datafolder, "USC00042294.anomalies", ".RData"))
+
+read_and_load_data.fun <- function(x){
+  print("This function might not be needed, waiting to see how the class does")
+}
+
+LoadData.fun <- function(datafolder){
+  if (file.exists(paste0(datafolder, "anamolies.RData" ))) {
+    load(file=paste0(datafolder, "anamolies.RData"))
+    print("RData file found and loaded")
+  } else {
+    print("RData file does not exist (Not if you have all functions in one Rmd file!).")
+}
+}
+
+if (file.exists(paste0(datapath, "anomalies.RData" ))) {
+  load(file=paste0(datapath, "anomalies.RData"))
+  print("RData file found and loaded")
+} else {
+  print("RData file does not exist.")
+}
+
+# test function
+# rm(USC00042294.anomalies)
+# datapath = "/home/mwl04747/RTricks/04_Regional_Climate_Trends/Data/SP24/"
+# LoadData.fun(datapath)
+
+#-------------------------------------------------------------------------------
+# Get Station Information, e.g. State Name, etc.
+station_names = c("ID",             #  1-11   Character   11
+                  "LATITUDE",       # 13-20   Real        8
+                  "LONGITUDE",      # 22-30   Real        9
+                  "ELEVATION",      # 32-37   Real        6
+                  "STATE",          # 39-40   Character   2
+                  "NAME",           # 42-71   Character
+                  "GSN FLAG",       # 73-75   Character
+                  "HCN/CRN FLAG",   # 77-79   Character
+                  "WMO ID"          # 81-85   Character
+)
+
+# Read ghcnd-stations.txt with fixed width format
+Stations = read.fwf("https://www.ncei.noaa.gov/pub/data/ghcn/daily/ghcnd-stations.txt", 
+                    col.names=station_names, fill=2,
+                    widths=c(11, -1, 8, -1, 9, -1, 6, -1, 2, -1, 30, -1, 3, -1, 3, -1, 5 ))
+
+# NOTE: Got to be a better way to get these data!
+
+# str(Stations) # Missing State Name
+
+# Now we'll get the state names for the states.
+State_names = c("STATE", #         1-2    Character 2
+                "STATE_NAME") #         4-50    Character 46
+States = read.fwf("https://www.ncei.noaa.gov/pub/data/ghcn/daily/ghcnd-states.txt", 
+                  col.names=State_names, fill=2, 
+                  widths=c(2, -1, 46))
+
+# str(States)
+# Merge the two datasets
+StateIDs = subset(Stations, select=c("ID", "STATE", "LATITUDE", "LONGITUDE", "ELEVATION", "NAME"))
+StateIDs = merge(StateIDs, States, by="STATE") # Add State Names
+
+# Look at the data
+
+#------------------------------------------------------------------------------
+# Function to analyze each month's trend
+monthlyTrend.fun <- function(station) {
+  # Disaggregate the data
+    TMAX = station$TMAX
+    TMIN = station$TMIN
+    PRCP = station$PRCP
+    TMAX.lm = lapply(1:12, function(i) {
+      lm(TMAX.a ~ YEAR, data=subset(TMAX, MONTH==i))
+    })  
+    TMIN.lm = lapply(1:12, function(i) {
+      lm(TMIN.a ~ YEAR, data=subset(TMIN, MONTH==i))
+    })
+    PRCP.lm = lapply(1:12, function(i) {
+      lm(PRCP.a ~ YEAR, data=subset(PRCP, MONTH==i))
+    })
+    
+    # Summarize the results
+    TMAX.summary <- lapply(TMAX.lm, summary)
+    TMIN.summary <- lapply(TMIN.lm, summary)
+    PRCP.summary <- lapply(PRCP.lm, summary)
+    
+    # Summarize lapply summary with a table
+    # including coefficients for each month and the p-value
+    TMAX.stats <- lapply(TMAX.summary, function(x) {
+      c(r.squared = x$r.squared,
+        x$coefficients[2,1:4]
+      )
+    })
+    TMIN.stats <- lapply(TMIN.summary, function(x) {
+      c(r.squared = x$r.squared,
+        x$coefficients[2,1:4]
+      )
+    })
+    PRCP.stats <- lapply(PRCP.summary, function(x) {
+      c(r.squared = x$r.squared,
+        x$coefficients[2,1:4]
+      )
+    })
+    TMAX.stats <- lapply(TMAX.stats, function(x) x[c(1:5)])
+    TMAX <- as.data.frame(do.call(rbind, TMAX.stats))
+    TMAX$MONTH <- 1:12; TMAX$ELEMENT <- "TMAX"
+    TMIN.stats <- lapply(TMIN.stats, function(x) x[c(1:5)])
+    TMIN <- as.data.frame(do.call(rbind, TMIN.stats))
+    TMIN$MONTH <- 1:12 ; TMIN$ELEMENT <- "TMIN"
+    PRCP.stats <- lapply(PRCP.stats, function(x) x[c(1:5)])
+    PRCP <- as.data.frame(do.call(rbind, PRCP.stats))
+    PRCP$MONTH <- 1:12; PRCP$ELEMENT <- "PRCP"
+    
+    # Return the results
+    return(rbind(TMAX[,c(7,6,2:5,1)], TMIN[,c(7,6,2:5,1)], PRCP[,c(7,6,2:5,1)]))
+}
+
+# test function
+# USC00042294.trends <- monthlyTrend.fun(USC00042294.anomalies)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Function to Plot the Trend, for a given month and element
+plotTrend2.fun <- function(station, element, month) {
+  if(element == "TMAX"){
+    list = 1
+    temp = subset(station[[list]], subset=MONTH==month)
+    daterange = range(temp$Ymd)
+    formula = as.formula("TMAX.a ~ Ymd")
+    ylab="Temperature Anomaly (C)"
+    main1="Maximum Temperature"
+  } else if(element == "TMIN"){
+    list = 2
+    temp = subset(station[[list]], subset=MONTH==month)
+    daterange = range(temp$Ymd)
+    formula = as.formula("TMIN.a ~ Ymd") 
+    ylab="Temperature Anomaly (C)"
+    main1="Minimum Temperature"
+  } else if(element == "PRCP"){
+    list = 3
+    temp = subset(station[[list]], subset=MONTH==month)
+    daterange = range(temp$Ymd)
+    formula = as.formula("PRCP.a ~ Ymd")
+    main1="Precipitation"
+    ylab="Precipitation Anomaly (mm)"
+  }
+  
+  temp.lm <- lm(formula, data=temp)
+  
+  sub=paste0("Trend: ", round(coef(temp.lm)[2]*365.25*100, 4), " C/100 Year; R-squared: ", round(summary(temp.lm)$r.squared, 3), "; p-value: ", round(summary(temp.lm)$coefficients[2,4], 3))
+  
+  station.text = sub("\\..*", "", deparse(substitute(station)))
+  citystate = paste0(trimws(subset(StateIDs, subset = ID == substr(station.text,1,11))$NAME, which = "right"), ", ", subset(StateIDs, subset = ID == substr(station.text,1,11))$STATE)
+  
+  main=paste0(main1, " Anomaly (", month.name[month], ") at ", sub("\\..*", "", citystate))
+  
+  par(mfrow=c(1,1), mar=c(4,4,2,2), oma=c(0,0,2,0), las=1)
+  plot(formula, data=temp, pch=19, 
+       ylab=ylab, xlab="Year", col="gray", cex=.5, 
+       main="")
+  mtext(main, side=3, line=2, cex=1.1)
+  mtext(sub, side=3, line=1, cex=.8)
+  abline(coef(temp.lm), col="red")
+}
+
+# test function
+# station = "USC00042294"
+# extracting name of station from the object name
+# sub("\\..*", "", deparse(substitute(USC00042294.anomalies)))
+# plotTrend.fun(USC00042294.anomalies, "TMAX", 6)
+# plotTrend.fun(USC00042294.anomalies, "TMIN", 7)
+# plotTrend.fun(USC00042294.anomalies, "PRCP", 1)
+
+
+# Testing the trend since 1975, where evidence suggest global
+# warming has been more pronounced or even accerating
+
+# USC00042294.1975 <- lapply(USC00042294.anomalies, function(x) subset(x, YEAR >= 1975))
+
+# test function
+# USC00042294.trends <- monthlyTrend.fun(USC00042294.1975)
+
+#subset(station[element][[list]], subset=MONTH==month)
+
+#TMIN.lm <- lm(TMIN.a ~ YEAR, data=subset(USC00042294.anamolies[[2]], MONTH==1))
+
+#coef(TMAX.lm)
+#summary(TMAX.lm)
+#plot(TMAX.a ~ YEAR, data=subset(USC00042294TMAX, MONTH==1), pch=19, col="gray", cex=.5)
+#abline(TMAX.lm, col="red")
+
+
+#-------------------------------------------------------------------------------
+#Loopwithfunction, eval=FALSE, echo=TRUE>>=
+
+MonthEvalStats.fun <- function(GSOM) {
+  sumstats = NA
+  for (m in 1:12){
+    TMIN.lm = lm(TMIN~Date, GSOM[GSOM$Month==m,])
+    TMAX.lm = lm(TMAX~Date, GSOM[GSOM$Month==m,])
+    PPT.lm  = lm(PPT~Date, GSOM[GSOM$Month==m,])
+    
+    sumstats = rbind(sumstats, 
+                     data.frame(Month = m, Param="TMIN", Slope = coef(TMIN.lm)[2], 
+                                r2 = summary(TMIN.lm)$r.squared, p_value= anova(TMIN.lm)$'Pr(>F)'[1]),
+                     data.frame(Month = m, Param="TMAX", Slope = coef(TMAX.lm)[2], 
+                                r2 = summary(TMAX.lm)$r.squared, p_value= anova(TMAX.lm)$'Pr(>F)'[1]),
+                     data.frame(Month= m, Param="PPT", Slope = coef(PPT.lm)[2], 
+                                r2 = summary(PPT.lm)$r.squared, p_value= anova(PPT.lm)$'Pr(>F)'[1]))
+    
+  } #end loop
+  
+  sumstats=data.frame(sumstats)[-1,]
+  rownames(sumstats)<-NULL
+  head(sumstats)
+  
+  sumstats$Symbol = ""
+  sumstats$Symbol[sumstats$p_value < 0.05] = "*"
+  sumstats$Symbol[sumstats$p_value < 0.01] = "**"
+  sumstats$Symbol[sumstats$p_value < 0.001] = "***"
+  return(sumstats)
+}
+
+# test function
+# sumstats = MonthEvalStats(GSOM[500:4000,])
+
+
+#-------------------------------------------------------------------------------
+# Functions for Evaluating drought
+
+# Function to calculate the SPI
+spi.fun <- function(x, scale=3, fit="pearson3") {
+  # Calculate the SPI
+  spi = droughtTools::spi(x, scale=scale, fit=fit)
+  return(spi)
+}
+
+# Function to Count the number of consecutive days of drought
+droughtCount.fun <- function(station=USC00040693, threshold=0.1) {
+  drought.df <- subset(station, ELEMENT=="PRCP")
+  x <- drought.df$VALUE
+  length(x)
+  # Count the number of days of drought
+  drought = x < threshold; str(drought)
+  drought = rle(drought); str(drought)
+  
+  rle.values <- as.data.frame(do.call(cbind, drought)); str(rle.values)
+  drought.df$Drought = as.logical(rep(rle.values$values, 
+                times = rle.values$lengths)); head(drought.df)
+  # change this to populate the end of the drough period MLH 2/4/2024
+  drought.df$Length = rep(rle.values$lengths, rle.values$lengths); head(drought.df)
+  if("Ymd" %in% names(drought.df) == FALSE){
+    drought.df$Ymd = as.Date(as.character(drought.df$DATE), format = "%Y%m%d")
+    drought.df$MONTH = as.numeric(format(drought.df$Ymd, "%m"))
+    drought.df$YEAR = as.numeric(format(drought.df$Ymd, "%Y"))
+  }
+  drought.df$WY = ifelse(drought.df$MONTH < 10, drought.df$YEAR, drought.df$YEAR+1)
+  #Drought = subset(station, Drought==TRUE); str(Drought)
+  #values = run.length$values  
+  
+  str(drought.df)
+
+  aggregate(drought.df$Drought, by=list(YEAR = drought.df$YEAR, MONTH = drought.df$MONTH), FUN=sum)
+  DroughtperYear = aggregate(drought.df$Drought, by=list(WY = drought.df$WY), FUN=sum)
+  RecordperYear = aggregate(drought.df$DATE, by=list(WY = drought.df$WY), FUN=length)
+
+  drought = merge(RecordperYear, DroughtperYear, by="WY"); head(drought)
+  drought$DroughtPerYear = round(drought$x.y/drought$x.x *100); head(drought)
+  
+  
+  return(drought)
+}
+
+# Test Function
+# USC00040693.drought <- droughtCount.fun(USC00040693, threshold=0.1)
+
+# Scratch Pad
+# str(USC00040693)
+# USC00040693.PRCP <- subset(USC00040693, ELEMENT=="PRCP", select=VALUE)[1]$VALUE
+# str(USC00040693.PRCP)
+# str(USC00040693.drought)
+
+# par(mfrow=c(1,1), las=1)
+# plot(DroughtPerYear ~ WY, data=USC00040693.drought, col="gray", 
+#     main="Percentage of Rainless Days", xlab="Water Year", ylab="Percent Days w/o Rain", pch=19, cex=.4)
+# abline(h=mean(USC00040693.drought$DroughtPerYear), col="red", lty=2)
+# abline(coef(lm(USC00040693.drought$DroughtPerYear ~ USC00040693.drought$WY)), col="blue", lty=2)
+            
+
+
+
+# What is a drought 10 days, 20 days, 40 days?
+
+#Drought.run.10 = aggregate(lengths~Year, data=Drought.run[Drought.run$lengths>=10,], sum)
+#Drought.run.20 = aggregate(lengths~Year, data=Drought.run[Drought.run$lengths>=20,], sum)
+# Drought.run.40 = aggregate(lengths~Year, data=Drought.run[Drought.run$lengths>=40,], sum)
+
+# if(Drought.run$lengths>100) {Drought.run.100 = aggregate(lengths~Year, data=Drought.run[Drought.run$lengths>=100,], sum)}
